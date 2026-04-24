3f06c09505901c33c9c992c63e9e38c25bc2b8da
Leonid Proskuryakov <lpr@inbox.ru>
2026-04-24 20:44:22 +0300

    1. StatClient; 2. Демонстрация в MainServiceApplication; 3. Эндпоинты в StatController с "заглушкой".

--- stat/stat-dto/src/main/java/ewm/stat/dto/HitDto.java
+++ stat/stat-dto/src/main/java/ewm/stat/dto/HitDto.java
@@ -1,6 +1,7 @@
 package ewm.stat.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +9,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
-public class EndpointHit {
+@AllArgsConstructor
+public class HitDto {
     @NotBlank
     String app;
     @NotBlank
