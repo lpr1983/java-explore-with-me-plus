ce57cd3d07faceebc7a6df2b88bac6ff4f4e4498
Ishxan <ishmsu2@inbox.ru>
2026-04-30 21:39:18 +0300

    Добавил users

--- main-service/src/main/java/ewm/main/MainServiceApplication.java
+++ main-service/src/main/java/ewm/main/MainServiceApplication.java
@@ -12,35 +12,5 @@ import org.springframework.context.annotation.ComponentScan;
 public class MainServiceApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(MainServiceApplication.class, args);
-
-        // Демонстрация работы StatClient (будет убрана на 2-м этапе)
-        /* StatClient statClient = context.getBean(StatClient.class);
-        try {
-            HitParams params = HitParams.builder()
-                    .uri("/event/1")
-                    .ip("192.168.1.1")
-                    .timestamp(LocalDateTime.now())
-                    .build();
-
-            statClient.saveHit(params);
-            log.info("saveHit");
-        } catch (StatClientException e) {
-            log.error("Ошибка работы statClient.saveHit: {}", e.getMessage());
-        }
-
-        try {
-            GetStatsParams params = GetStatsParams.builder()
-                    .start(LocalDateTime.now().minusHours(1))
-                    .end(LocalDateTime.now().plusHours(1))
-                    .uris(List.of("/event/1", "/event/2"))
-                    .unique(false)
-                    .build();
-
-            List<StatDto> statResult = statClient.getStats(params);
-            log.info("getStats, result: {}", statResult);
-        } catch (StatClientException e) {
-            log.error("Ошибка работы statClient.getStats: {}", e.getMessage());
-        } */
     }
-
 }
