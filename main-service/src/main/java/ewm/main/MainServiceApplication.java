f2f529e4cc78fc2c6f4138af9e42a29542d2fb50
Ishxan <ishmsu2@inbox.ru>
2026-04-30 21:28:21 +0300

    Добавил users

--- main-service/src/main/java/ewm/main/MainServiceApplication.java
+++ main-service/src/main/java/ewm/main/MainServiceApplication.java
@@ -2,7 +2,8 @@ package ewm.main;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
